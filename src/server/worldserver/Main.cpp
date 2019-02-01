e40ce5a6a69afacc5cb79734eebd0433bf2b9828
welder1976 <zemetskia@gmail.com>
2019-02-01 21:06:25 +0300

    rename

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -77,8 +77,8 @@ namespace fs = boost::filesystem;
 #ifdef _WIN32
 #include "ServiceWin32.h"
 char serviceName[] = "worldserver";
-char serviceLongName[] = "TrinityCore world service";
-char serviceDescription[] = "TrinityCore World of Warcraft emulator world service";
+char serviceLongName[] = "MagicStormTeam world service";
+char serviceDescription[] = "MagicStormTeam World of Warcraft emulator world service";
 /*
  * -1 - not in service mode
  *  0 - stopped
