20f2859a19ff99471771156cdf490d52fc1a2de9
welder1976 <nasist@tut.by>
2019-02-09 19:51:23 +0300

    the .lo quest crash fix for Ashamane Legion

--- sql/MSTeam/2019_02_04_01.sql
+++ sql/MSTeam/2019_02_04_01.sql
@@ -16,3 +16,5 @@ INSERT INTO item_script_names VALUES
 (147319, 'loot_item_gloves_of_the_foregone'),
 (147320, 'loot_item_gloves_of_the_foregone'),
 (147321, 'loot_item_gloves_of_the_foregone');
+
+UPDATE `trinity_string` SET `content_default`='%u - |cffffffff|Hquest:%u:%d:%d:%d:%d|h[%s]|h|r %s' WHERE `entry`=513;
